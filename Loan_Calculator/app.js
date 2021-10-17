b57a2d134ef7bb1ca7fca066b14d4bfb93b09c60
Ritchie Cervantes <ritchiecervantes@Ritchies-MacBook-Pro.local>
2021-10-16 21:42:56 -0500

    calculate payment

---
+++ Loan_Calculator/app.js
@@ -0,0 +1,32 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+function calculateResults(e){
+    // UI variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+        console.log('Please use valid numbers');
+    }
+    
+    e.preventDefault();
+}
+
